1dedd1a8cadf486d4540f4f01a5217da8b83cf5f
Ernesto Arroyo <earroyo@heytrade.com>
2024-08-25 17:48:12 +0200

    Initial version

---
+++ target/classes/com/earroyoron/chess_club/ChessClubApplication.kt
@@ -0,0 +1,11 @@
+package com.earroyoron.chess_club
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class ChessClubApplication
+
+fun main(args: Array<String>) {
+	runApplication<ChessClubApplication>(*args)
+}
---
+++ target/test-classes/com/earroyoron/chess_club/ChessClubApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.earroyoron.chess_club
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class ChessClubApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
